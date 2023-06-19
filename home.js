84b07f052216e91c37861116ace0b621e3327613
Tai Merkle <taiht221@gmail.com>
2023-06-19 18:33:26 +0700

    update animation

--- home.js
+++ home.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
-    var idInput = 0
-    var noteList = document.querySelectorAll('.note-list')
+  AOS.init()
+  var idInput = 0
+  var noteList = document.querySelectorAll('.note-list')
 
-    $('#banner-form').on('submit', function (e) {
-        e.preventDefault()
-        // console.log(noteList[idInput])
-        console.log($('input:first').val())
-        noteList[idInput].innerHTML = `<p class="note-text">${$('input:first').val()}</p>`
-        idInput++
-        $('input:first').val('')
-        console.log(idInput)
-        if (idInput == 10) idInput = 0
-    })
-});
-
+  $('#banner-form').on('submit', function (e) {
+    e.preventDefault()
+    // console.log(noteList[idInput])
+    console.log($('input:first').val())
+    noteList[idInput].innerHTML = `<p class="note-text">${$('input:first').val()}</p>`
+    idInput++
+    $('input:first').val('')
+    console.log(idInput)
+    if (idInput == 10) idInput = 0
+  })
+})
