2f3ddc067ab20ab2dcb44bc0374f41d3ae4297a7
Huynh Tien Tai <taiht221@gmail.com>
2023-09-18 21:43:22 +0700

    update

--- home.js
+++ home.js
@@ -185,4 +185,25 @@ $(document).ready(function () {
 
   if (location.pathname != "/") $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
 
+  // set the modal menu element
+  const $targetEl = document.getElementById('modalEl');
+
+  // options with default values
+  const options = {
+    placement: 'bottom-right',
+    backdrop: 'dynamic',
+    backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+    closable: true,
+    onHide: () => {
+      console.log('modal is hidden');
+    },
+    onShow: () => {
+      console.log('modal is shown');
+    },
+    onToggle: () => {
+      console.log('modal has been toggled');
+    }
+  };
+
+  modal.show();
 })
